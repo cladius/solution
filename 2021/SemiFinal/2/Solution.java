d58312025afa2992e72ca646ca4141bb514a76c6
Cladius Fernando <cladius@users.noreply.github.com>
2022-04-15 15:22:03 +0530

    Version 4 Score 50

--- 2021/SemiFinal/2/Solution.java
+++ 2021/SemiFinal/2/Solution.java
@@ -2,17 +2,16 @@ import java.io.*;
 import java.util.*;
 
 /**
- * Semi Final 2 - Version 2 Score 20
- * 2 passed
- * 1 mismatch
- * 7 TLEs
+ * Semi Final 2 - Version 4 Score 50
+ * 5 passed
+ * 5 TLEs
+ *
+ * Used boolean array for visited nodes as opposed to Set of nodes
  *
  * @author cladius_fernando
  *
  */
 public class Solution {
-//    static final boolean DEBUG = false;
-
 
     /*
 5 5
@@ -29,13 +28,37 @@ public class Solution {
 
      */
 
+//    static final boolean DEBUG = false;
     static final boolean DEBUG = true;
 
-    private static int [][] roadCosts;
-    private static Set<Integer> visitedNodes;
+    private static Map<Integer, Map<Integer, Integer>> roadCosts;
+    private static boolean [] visitedNodes;
     private static int numberOfHouses;
     private static boolean keepVisiting = true;
     private static final String NOT_POSSIBLE = "NOT POSSIBLE";
+    private static PriorityQueue<Edge> rootHouseNeighbors;
+
+    /*
+     * To use with the Q.
+     * houseIndex => the destination
+     * weight => cost to reach that destination from rootHouse
+     */
+    static class Edge{
+        public Edge(int houseIndex, int weight) {
+            this.houseIndex = houseIndex;
+            this.weight = weight;
+        }
+
+        public int houseIndex, weight;
+    }
+
+    static class EdgeComparator implements Comparator<Edge>{
+
+        @Override
+        public int compare(Edge o1, Edge o2) {
+            return o1.weight - o2.weight;
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
@@ -44,8 +67,9 @@ public class Solution {
             numberOfHouses = parseInt(input[0]);
             int numberOfRoads = parseInt(input[1]);
 
-            roadCosts = new int[numberOfHouses][numberOfHouses];
-            visitedNodes = new HashSet<>();
+            roadCosts = new HashMap<>();
+            visitedNodes = new boolean[numberOfHouses];
+            rootHouseNeighbors = new PriorityQueue<>(new EdgeComparator()); //Need to create with some initialCapacity?
 
             for(int i = 0; i < numberOfRoads; i++) {
                 input = reader.readLine().split(" ");
@@ -63,8 +87,24 @@ public class Solution {
         //Since indices start with 0 and not 1, decrementing source and destination
         source--;
         destination--;
-        roadCosts[source][destination] = weight;
-        roadCosts[destination][source] = weight;
+
+        addEdge(source, destination, weight);
+        addEdge(destination, source, weight);
+    }
+
+    private static void addEdge(int source, int destination, int weight) {
+        Map<Integer, Integer> edges = roadCosts.get(source);
+        if(edges == null) {
+            edges = new LinkedHashMap<>();
+            roadCosts.put(source, edges);
+        }
+
+        //If adding for source as 0, insert in Priority Q
+        if(source == 0){
+            rootHouseNeighbors.add(new Edge(destination, weight));
+        }
+
+        edges.put(destination, weight);
     }
 
     private static int parseInt(String s) {
@@ -76,9 +116,9 @@ public class Solution {
         //If there is just 1 house - there is no cost
         if(numberOfHouses == 1)
             System.out.println(0);
-        //If there are just 2 houses then use the road cost.
+            //If there are just 2 houses then use the road cost.
         else if(numberOfHouses == 2)
-            System.out.println(roadCosts[0][1]);
+            System.out.println(getRoadCost(0, 1));
         else {
             //Let the calculations begin
             do {
@@ -87,7 +127,7 @@ public class Solution {
                     visit(houseIndex);
             } while (keepVisiting);
 
-            int finalCost = roadCosts[0][numberOfHouses - 1];
+            int finalCost = getRoadCost(0, numberOfHouses - 1);
             if (finalCost == 0)
                 System.out.println(NOT_POSSIBLE);
             else
@@ -95,23 +135,35 @@ public class Solution {
         }
     }
 
+    private static int getRoadCost(int source, int destination){
+        Map<Integer, Integer> roads = roadCosts.get(source);
+        Integer cost;
+        if(roads != null) {
+            cost = roads.get(destination);
+            if(cost != null)
+                return cost;
+        }
+        return 0; //Should it be this?
+    }
+
     private static void visit(int houseIndex) {
         //Add house to visited set
-        visitedNodes.add(houseIndex);
-        int costFromRootToThisHouse = roadCosts[0][houseIndex];
+        visitedNodes[houseIndex] = true;
+        int costFromRootToThisHouse = getRoadCost(0, houseIndex);
 
         int currentCostFromRootToOtherHouse, newCostFromRootToOtherHouse, costFromThisHouseToOtherHouse;
 
         /*
          * Check if there is an edge from 'houseIndex' to any "other" houses
          * If cost(root, houseIndex) + cost (houseIndex, otherHouse) < cost(root, otherHouse) => set new min cost
+         *
          */
         for(int j = 0; j < numberOfHouses; j++){
             //If other house is already visited - skip
-            if(visitedNodes.contains(j))
+            if(visitedNodes[j])
                 continue;
 
-            costFromThisHouseToOtherHouse = roadCosts[houseIndex][j];
+            costFromThisHouseToOtherHouse = getRoadCost(houseIndex, j);
 
             //If there is no route from this house to the other house
             if(costFromThisHouseToOtherHouse == 0)
@@ -121,13 +173,13 @@ public class Solution {
             if(costFromThisHouseToOtherHouse < 0)
                 costFromThisHouseToOtherHouse = 0;
 
-            currentCostFromRootToOtherHouse = roadCosts[0][j];
+            currentCostFromRootToOtherHouse = getRoadCost(0, j);
 
             newCostFromRootToOtherHouse = costFromRootToThisHouse + costFromThisHouseToOtherHouse;
 
             //Relax if possible
             if(currentCostFromRootToOtherHouse == 0 || newCostFromRootToOtherHouse < currentCostFromRootToOtherHouse)
-                roadCosts[0][j] = newCostFromRootToOtherHouse;
+                addEdge(0, j, newCostFromRootToOtherHouse);
         }
 
         //Check if visited house is the last house?
@@ -140,29 +192,14 @@ public class Solution {
      * Find the node with least cost - and which is not already visited.
      */
     private static int findNextNodeToVisit() {
-        int houseIndex = 0;
-        int minWeight = -1;
-        int nextNode = -1;
+        //Poll the Q
+        Edge nextNode = rootHouseNeighbors.poll();
 
-        for(; houseIndex < numberOfHouses; houseIndex++){
-            //If the node has already been visited - let's skip it.
-            if(visitedNodes.contains(houseIndex)){
-                continue;
-            }
-
-            int costFromRoot = roadCosts[0][houseIndex];
-            //If there is no minWeight set so far or this weight is less than earlier selected minWeight
-            //Also if costFromRoot == 0 => there is no path yet till this node from root.
-            if(minWeight == -1 || (costFromRoot != 0 && costFromRoot < minWeight) ) {
-                minWeight = costFromRoot;
-                nextNode = houseIndex;
-            }
-        }
-
-        if(nextNode == -1)
+        if(nextNode == null) {
             keepVisiting = false;
-
-        return nextNode;
+            return -1;
+        }else
+            return nextNode.houseIndex;
     }
 
 
