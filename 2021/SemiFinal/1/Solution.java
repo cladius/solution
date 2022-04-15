213f96a15d4fe00d0fdf494e4cebd0dff67a5709
Cladius Fernando <cladius@users.noreply.github.com>
2022-04-15 15:02:20 +0530

    Version 3 Score 100

--- 2021/SemiFinal/1/Solution.java
+++ 2021/SemiFinal/1/Solution.java
@@ -2,15 +2,15 @@ import java.io.*;
 import java.util.*;
 
 /**
- * Semi Final 1 - Version 2 Score 80 - TLEs went away - but 2 mismatch failures
- * 
+ * Semi Final 1 - Version 3 Score 100 
+ *
  * @author cladius_fernando
  *
  */
 public class Solution {
 
 //	static final boolean DEBUG = true;
-	static final boolean DEBUG = false;
+    static final boolean DEBUG = false;
 
 	public static void main(String[] args) throws Exception {
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
@@ -24,7 +24,7 @@ public class Solution {
 
 			input = reader.readLine();
 			String[] packetSizes = input.split(" ");
-			
+
 			for (; test_case_index < test_case_count; test_case_index++) {
 				packetSize = Long.parseLong(packetSizes[test_case_index]);
 				totalMoves += processTestCase(packetSize);
@@ -39,48 +39,52 @@ public class Solution {
 	private static long processTestCase(long packetSize) {
 		if(packetSize == 1L)
 			return 1L;
-		List<Long> divisors = getDivisors(packetSize);
-		return calculateMoves(divisors) + packetSize;
+		return calculateMoves(packetSize);
 	}
 
-	
-	private static long calculateMoves(List<Long> divisors) {
+
+	private static long calculateMoves(long number) {
+		List<Long> divisors = getDivisors(number);
+		if(DEBUG)
+			System.out.println(divisors);
 		int size = divisors.size();
 		int i = size - 1;
-		int moves = 1;
-		
+		long moves = 1;
+
 		long lastProduct = 1;
-		
-		for(; i > 0; i--) {
+
+		for (; i > 0; i--) {
 			lastProduct = lastProduct * divisors.get(i);
 			moves += lastProduct;
 		}
-		
-		return moves;
+
+		if(DEBUG)
+			System.out.println(moves + number);
+		return moves + number;
 	}
 
 	private static List<Long> getDivisors(long number) {
 		List<Long> divisors = new ArrayList<>();
 
-		long maxLimit = number/2 + 1;
+		long maxLimit = number / 2 + 1;
+
 		long divisor = 2;
-		
-		for(; divisor <= maxLimit; ) {
+		for (; divisor < maxLimit; ) {
 			//If the number is divisible then divide and reset the var
-			if(number % divisor == 0 && number != divisor) {
+			if (number % divisor == 0 && number != divisor) {
 				divisors.add(divisor);
 				number = number / divisor;
-				maxLimit = number / divisor;
-			}else {
+				maxLimit = number / divisor + 1;
+			} else {
 				divisor++;
-				maxLimit = number / divisor;
+				maxLimit = number / divisor + 1;
 			}
 		}
-		
+
 		divisors.add(number);
-		
+
 		return divisors;
 	}
-	
+
 
 }
